b7a8b15cc920af786a3b3fa6507dd88a17887eca
서보빈 <121992713+Bbins-dev@users.noreply.github.com>
2023-02-24 05:38:46 +0900

    Add files via upload

---
+++ public_html/assets/js/index.js
@@ -0,0 +1,48 @@
+/*!
+=========================================================
+* Meyawo Landing page
+=========================================================
+
+* Copyright: 2019 DevCRUD (https://devcrud.com)
+* Licensed: (https://devcrud.com/licenses)
+* Coded by www.devcrud.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+// navbar toggle
+$('#nav-toggle').click(function(){
+    $(this).toggleClass('is-active')
+    $('ul.nav').toggleClass('show');
+});
+
+$(".popupModalVideo a").click(function() {
+    $(".video_modal_popup").addClass("reveal"),
+    $(".video_modal_popup .video-wrapper").remove(),
+    $(".video_modal_popup").append("<div class='video-wrapper'><iframe width='560' height='315' src='https://youtube.com/embed/" + $(this).data("video") + "?rel=0&playsinline=1&autoplay=1' allow='autoplay; encrypted-media' allowfullscreen></iframe></div>")
+}),
+$(".video_modal_popup-closer").click(function() {
+    $(".video_modal_popup .video-wrapper").remove(),
+    $(".video_modal_popup").removeClass("reveal")
+});
